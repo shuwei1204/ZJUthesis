baf5f4701e39d69781a25466f2c231a2c83a919c
shuwei1204 <shuwei1204@163.com>
2016-08-07 19:17:46 +0800

    2016-8-7

--- ZJUthesis/rookie_ZJUthesis_v2.1.0_GBKUTF/gbkver/makethesis.bat
+++
@@ -1,6 +0,0 @@
-latex --src-specials --synctex=-1 论文模版示例
-makeindex 论文模版示例.idx
-bibtex 论文模版示例
-latex --src-specials --synctex=-1 论文模版示例
-latex --src-specials --synctex=-1 论文模版示例
-dvipdfmx -p a4 论文模版示例
